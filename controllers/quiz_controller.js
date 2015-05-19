82c21254bd3b255c6fc78f57c2e3a00b58d3b0f2
Luis Llorente <luis.lltovar@hotmail.com>
2015-05-19 14:45:58 +0200

    Buscador de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,16 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+if(req.query.search){
+  models.Quiz.findAll({where: ["pregunta like ?", '%' + req.query.search + '%']}).then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes});
+  });
+}
+else{
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes});
+  });
+}
  };
 
 // GET /quizes/:id
